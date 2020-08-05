667df9ebcca387c11fcde3a6eacf197aa475fff3
zhaoys <windows>
2020-08-05 23:46:59 +0800

    自定义Gradle插件

---
+++ deps/src/main/java/com/centa/salehouse/RetrofitUtil.java
@@ -0,0 +1,26 @@
+package com.centa.salehouse;
+
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitUtil {
+    private RetrofitUtil() {
+    }
+
+    public static Retrofit get(String baseUrl) {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.level(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient httpClient = new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
+        return new Retrofit.Builder().baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient).build();
+    }
+}
