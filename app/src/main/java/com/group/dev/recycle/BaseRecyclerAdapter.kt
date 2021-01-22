d1dfaeab1b8f77179130fe6ed89ad4098dddab1b
zhaoys <zhaoys@centaline.com.cn>
2021-01-22 16:49:40 +0800

    modify rootProject name

---
+++ app/src/main/java/com/group/dev/recycle/BaseRecyclerAdapter.kt
@@ -0,0 +1,10 @@
+package com.group.dev.recycle
+
+/**
+ * 描述:
+ *
+ * author zys
+ * create by 2021/1/22
+ */
+//class BaseRecyclrAdapter:RecyclerView.Adapter<RecyclerVH>() {
+//}
---
+++ app/src/main/java/com/group/dev/recycle/RecyclerVH.kt
@@ -0,0 +1,15 @@
+package com.group.dev.recycle
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * 描述:
+ *
+ * author zys
+ * create by 2021/1/22
+ */
+class RecyclerVH(itemView:View):RecyclerView.ViewHolder(itemView) {
+
+
+}
