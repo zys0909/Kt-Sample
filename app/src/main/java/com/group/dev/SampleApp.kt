d2d95a038e5f5d57822f168dfa26ac369f37f740
zhaoys <zhaoys@centaline.com.cn>
2021-03-23 13:41:43 +0800

    flow 协程测试

--- app/src/main/java/com/group/dev/SampleApp.kt
+++ app/src/main/java/com/group/dev/SampleApp.kt
@@ -2,6 +2,7 @@ package com.group.dev
 
 import android.app.Application
 import com.group.common.core.App
+import timber.log.Timber
 
 /**
  * 描述:
@@ -14,6 +15,7 @@ class SampleApp : Application() {
     override fun onCreate() {
         super.onCreate()
 //        App.init(this,resources.getBoolean(R.bool.debug_mode))
+        Timber.plant(Timber.DebugTree())
         App.init(this,true)
 
     }
--- app/src/main/java/com/group/dev/main/HomeFragment2.kt
+++ app/src/main/java/com/group/dev/main/HomeFragment2.kt
@@ -1,10 +1,19 @@
 package com.group.dev.main
 
 import android.view.View
+import androidx.lifecycle.liveData
 import com.group.common.adapter.ItemCell
 import com.group.common.base.BaseSampleFragment
 import com.group.common.ext.openActivity
 import com.group.dev.ui.wanandroid.WanMainActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import timber.log.Timber
+import kotlin.coroutines.coroutineContext
 
 /**
  * 描述:
@@ -13,15 +22,56 @@ import com.group.dev.ui.wanandroid.WanMainActivity
  * create by 2021/2/26
  */
 class HomeFragment2 : BaseSampleFragment() {
+
+    override fun init(view: View) {
+        setTitle("BBBB", false)
+    }
+
     override fun initList(): List<ItemCell> {
         val list = mutableListOf<MainCell>()
         list.add(MainCell("WanAndroid") {
             openActivity<WanMainActivity>()
         })
+        list.add(MainCell("协程测试") {
+
+        })
         return list
     }
 
-    override fun init(view: View) {
-        setTitle("BBBB", false)
+    fun test(temp: String, block: String.() -> Unit) {
+
+        block(temp)
+
+        block.invoke(temp)
+
+        temp.block()
+    }
+
+
+    private fun test() {
+        val block: suspend () -> String = {
+            Timber.tag("测试TAG").i("block -> %s,协程-%s", Thread.currentThread().name, coroutineContext)
+            delay(3000)
+            "执行异步操作"
+        }
+        liveData {
+            flow {
+                Timber.tag("测试TAG").i("flow -> %s,协程-%s", Thread.currentThread().name,
+                    coroutineContext
+                )
+                emit(block.invoke())
+            }.catch {
+                Timber.tag("测试TAG").i("catch -> %s,协程-%s", Thread.currentThread().name,
+                    kotlin.coroutines.coroutineContext
+                )
+                emit("出错了")
+            }.flowOn(Dispatchers.IO).collect {
+                Timber.tag("测试TAG").i("collect -> %s,协程-%s", Thread.currentThread().name, coroutineContext)
+                emit(it)
+            }
+        }.observe(this) {
+            Timber.tag("测试TAG").i("observe -> %s", Thread.currentThread().name)
+            Timber.tag("测试TAG").i("test -> %s", it)
+        }
     }
 }
